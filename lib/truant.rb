7f4f652c8d5b56a260c36810e9cc67cfc129eb1e
Kyle Bowen <kyle.m.bowen@gmail.com>
2024-01-20 15:46:32 -0700

    Init gem using bundler

---
+++ lib/truant.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "truant/version"
+
+module Truant
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/truant_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Truant do
+  it "has a version number" do
+    expect(Truant::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
